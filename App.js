22867087cd8d2b7335221a492a6699c707de86f2
navtej21 <nairnavtej@gmail.com>
2024-04-16 22:18:10 +0530

    final changes

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, Button, Linking } from 'react-native';
 
 const HomeScreen = () => {
+  const [backgroundColor,setBackgroundcolor]=useState("red");
+
+  const HandleColorChange=()=>{
+    const newcolor=backgroundColor==='red'?'blue':'red';
+    setBackgroundcolor(newcolor);
+  }
   const handleExploreDocs = () => {
     Linking.openURL('https://reactnative.dev/docs/getting-started');
   };
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { backgroundColor }]}>
       <Text style={styles.title}>My React Native App</Text>
       <Text style={styles.name}>Navtej S Nair</Text>
       <View style={styles.buttonContainer}>
         <Button title="Check Docs" onPress={handleExploreDocs} />
+        <Button title="Check color" onPress={HandleColorChange}/>
       </View>
     </View>
   );
